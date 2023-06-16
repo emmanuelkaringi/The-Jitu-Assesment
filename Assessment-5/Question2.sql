0e3df114946987c1ee14762e7c50786b375e5ff0
Immah <emmanuelkaringi@gmail.com>
2023-06-16 10:08:19 +0300

    minor changes

--- Assessment-5/Question2.sql
+++ Assessment-5/Question2.sql
@@ -1,7 +1,7 @@
 -- Create a view that returns the total quantity and sales amount for each product.
 
-CREATE VIEW Total_Quantity_Sales AS
-Select product_id, SUM(quantity) AS total_quantity, SUM(quantity * list_price) AS sales_amount
+CREATE VIEW sales.Total_Quantity_Sales AS
+SELECT product_id, SUM(quantity) AS total_quantity, SUM(quantity * list_price) AS sales_amount
 FROM sales.order_items
 
-SELECT * FROM Total_Quantity_Sales;
+SELECT * FROM sales.Total_Quantity_Sales;
