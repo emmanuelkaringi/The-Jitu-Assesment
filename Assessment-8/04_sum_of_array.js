3b88e51158260aeef671a72947914acd57cc6bbb
Immah <emmanuelkaringi@gmail.com>
2023-06-30 11:25:34 +0300

    add array in question 4

--- Assessment-8/04_sum_of_array.js
+++ Assessment-8/04_sum_of_array.js
@@ -4,8 +4,9 @@ const array1 = [2, "11", 3, "a2", false, 5, 7, 1]
 
 const array2 =[2, 3, 0, 5, 7, 8, true, false]
 
+const array3 = [6, 9, true, 3, 0, false, 'Random', 6, 7, 'String']
 console.log(
-    array2.map(function(elt){
+    array3.map(function(elt){
         return /^\d+$/.test(elt) ? parseInt(elt):0;
     })
     .reduce(function(a,b){
