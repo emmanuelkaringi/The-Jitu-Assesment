a6ace6e3c037c9590af98162297b5326462b83fe
Immah <emmanuelkaringi@gmail.com>
2023-06-30 11:31:44 +0300

    add comments in question 4 for future reference

--- Assessment-8/04_sum_of_array.js
+++ Assessment-8/04_sum_of_array.js
@@ -6,9 +6,12 @@ const array2 =[2, 3, 0, 5, 7, 8, true, false]
 
 const array3 = [6, 9, true, 3, 0, false, 'Random', 6, 7, 'String']
 console.log(
+    // Convert each element in the array to a number if it is a string that matches the regular expression. Converts any non number to 0.
     array3.map(function(elt){
         return /^\d+$/.test(elt) ? parseInt(elt):0;
     })
+
+    // Add up all of the numbers in the array.
     .reduce(function(a,b){
         return a+b
     })
